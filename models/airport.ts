8382d284d2a350c011e380edd27438d99b76b47b
safakadir <safakadir@gmail.com>
2023-02-25 15:55:20 +0300

    backend api altered

--- models/airport.ts
+++ models/airport.ts
@@ -1,10 +1,35 @@
 import airports from '../data/airports.json'
 import Airport from '../types/airport'
 
+const BATCH_SIZE = 10
+
 export const findAirportByIata = async (iata: string): Promise<Airport | undefined> => {
   return airports.find(airport => airport.iata === iata.toUpperCase())
 }
 
 export const allAirports = async (): Promise<Airport[]> => {
   return airports.slice(0,10)
+}
+
+export const getAirportsAfter = async (after: number): Promise<Airport[]> => {
+  return airports.slice(after, after+BATCH_SIZE)
+}
+
+export const searchAndGetAirportsAfter = async (search: string, after: number): Promise<Airport[]> => {
+  const result = []
+  const searchLower = search.toLowerCase()
+  for(let airport of airports) {
+    if(!isMatches(airport, searchLower)) continue
+    if(result.length < BATCH_SIZE) result.push(airport)
+    if(result.length == BATCH_SIZE) break
+  }
+  return result
+}
+
+const isMatches = (airport: Airport, search: string): boolean => {
+  if(airport.name.toLowerCase().includes(search)) return true
+  if(airport.city.toLowerCase().includes(search)) return true
+  if(airport.iata.toLowerCase().includes(search)) return true
+  if(airport.country.toLowerCase().includes(search)) return true
+  return false
 }
--- pages/api/airports.ts
+++ pages/api/airports.ts
@@ -1,9 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import { allAirports } from '../../models/airport'
+import { getAirportsAfter, searchAndGetAirportsAfter } from '../../models/airport'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const airports = await allAirports()
+  
+  let after = parseInt(req.query.after as string)
+  after = Number.isNaN(after) ? 0 : after
+  const search = req.query.search as string
+
+  let airports
+  if(!search) {
+    airports = await getAirportsAfter(after)
+  }
+  else {
+    airports = await searchAndGetAirportsAfter(search, after)
+  }
 
   res.status(200).json(airports)
 }
