b15fd52b3e5034a8761e193b767a10282e867228
safakadir <safakadir@gmail.com>
2023-02-25 15:56:06 +0300

    type error fixed

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,6 @@
-const Layout: React.FC = ({ children }) => {
+import { PropsWithChildren } from "react"
+
+const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return <div className='container mx-auto py-5 px-5 md:px-0 md:py-10'>
     {children}
   </div>
