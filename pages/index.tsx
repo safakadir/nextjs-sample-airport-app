5e4175c91bf8e50c6f0bf259bb217d1ceff55d21
safakadir <safakadir@gmail.com>
2023-02-26 15:36:16 +0300

    added hover effects to buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ const Page: NextPage = () => {
 
       <div className='relative mt-10'>
         <input type='text' placeholder='Start Typing...' className='bg-gray-50 border rounded w-full p-3' value={searchText} onChange={handleSearchChange} onKeyDown={handleSearchKeyDown} />
-        {searchText !== '' && <span className='absolute p-3 top-0 right-0 my-auto text-neutral-400 cursor-pointer' onClick={handleClearSearchBox}>X</span>}
+        {searchText !== '' && <span className='absolute m-2.5 py-1 px-2.5 rounded-full right-0 text-neutral-400 cursor-pointer hover:bg-gray-100' onClick={handleClearSearchBox}>X</span>}
       </div>
 
       <div className='flex mt-10'>
@@ -46,8 +46,8 @@ const Page: NextPage = () => {
         <div className='bg-blue-400 rounded-2xl text-white text-sm font-bold px-2 py-1'>{dataCount}</div>
         {!!currentSearch &&
           <div className='w-full flex justify-end items-end text-sm mr-3'>
-            Showing results for<span className='font-bold ml-1'>{currentSearch}</span>
-            <span className='text-neutral-400 cursor-pointer font-bold ml-2' onClick={handleClearSearch}>X</span>
+            Showing results for<span className='font-bold mx-1'>{currentSearch}</span>
+            <span className='relative text-neutral-400 cursor-pointer font-bold hover:bg-gray-50 py-1 px-2 top-1 rounded-full' onClick={handleClearSearch}>X</span>
           </div>
         }
       </div>
@@ -66,7 +66,7 @@ const Page: NextPage = () => {
 
       <div className='flex justify-end items-center mt-3'>
         <span className='mr-3'>Showing {airports.length} of {dataCount} records</span>
-        {hasMore && <div onClick={loadmore} className='block text-neutral-500 text-sm cursor-pointer px-3 py-2 border rounded-3xl w-24'>Load More</div>}
+        {hasMore && <div onClick={loadmore} className='block text-neutral-500 text-sm cursor-pointer px-3 py-2 border rounded-3xl w-24 hover:bg-gray-50'>Load More</div>}
       </div>
     </div>
   </Layout>
